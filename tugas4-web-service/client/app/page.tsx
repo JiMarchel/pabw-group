25db0356ad1ea3a09ef0ee4dac4615b2f897e453
JiMarchel <jmarchel100@gmail.com>
2025-01-06 07:03:42 +0700

    update web service

--- tugas4-web-service/client/app/page.tsx
+++ tugas4-web-service/client/app/page.tsx
@@ -4,20 +4,20 @@ import { CarFormCreate } from "@/components/car-form-create";
 
 
 const Home = async () => {
-	const getData = await fetch("http://localhost:3000/get-cars");
-	const data = await getData.json()
+  const getData = await fetch("http://localhost:3000/get-cars");
+  const data = await getData.json()
 
-	return (
-		<div className="px-10">
-			<div>
-				<h1 className="my-10 text-4xl font-bold">Monolith | Cars Project</h1>
-			</div>
-			<div className="mb-3">
-				<CarFormCreate title="Create" className="bg-primary w-fit text-white rounded font-medium px-5 py-2 hover:bg-primary/90" />
-			</div>
-			<DataTable data={data.data} columns={columns} />
-		</div>
-	);
+  return (
+    <div className="px-10">
+      <div>
+        <h1 className="my-10 text-4xl font-bold">Web Service | Cars Project</h1>
+      </div>
+      <div className="mb-3">
+        <CarFormCreate title="Create" className="bg-primary w-fit text-white rounded font-medium px-5 py-2 hover:bg-primary/90" />
+      </div>
+      <DataTable data={data.data.reverse()} columns={columns} />
+    </div>
+  );
 }
 
 export default Home;
