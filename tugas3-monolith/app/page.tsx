23e73b514e36905d2ac46bf556a0ef7018569fa0
JiMarchel <jmarchel100@gmail.com>
2025-01-06 10:24:40 +0700

    add pagination & env

--- tugas3-monolith/app/page.tsx
+++ tugas3-monolith/app/page.tsx
@@ -4,20 +4,23 @@ import { prisma } from "@/lib/prisma";
 import { CarForm } from "@/components/car-form";
 import { createCar } from "@/action/create";
 
-
 const Home = async () => {
-  const data = await prisma.cars.findMany()
+  const data = await prisma.cars.findMany();
   return (
     <div className="px-10">
       <div>
         <h1 className="my-10 text-4xl font-bold">Monolith | Cars Project</h1>
       </div>
       <div className="mb-3">
-        <CarForm title="Create" className="bg-primary w-fit text-white rounded font-medium px-5 py-2 hover:bg-primary/90" action={createCar} />
+        <CarForm
+          title="Create"
+          className="bg-primary w-fit text-white rounded font-medium px-5 py-2 hover:bg-primary/90"
+          action={createCar}
+        />
       </div>
-      <DataTable data={data} columns={columns} />
+      <DataTable data={data.reverse()} columns={columns} />
     </div>
   );
-}
+};
 
 export default Home;
